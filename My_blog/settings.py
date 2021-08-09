d5b2298ca3a5ffe91590374eefa9948655951961
sabari ravikumar <sabariravissl20@gmail.com>
2021-08-09 15:27:30 +0530

    env commit

--- My_blog/settings.py
+++ My_blog/settings.py
@@ -175,5 +175,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_ID')
-EMAIL_HOST_PASSWORD =os.getenv('EMAIL_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER',default='')
+EMAIL_HOST_PASSWORD =config('EMAIL_HOST_PASSWORD',default='')
